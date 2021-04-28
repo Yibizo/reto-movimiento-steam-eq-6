235d8b95976d46ea8ea7493ce8a4c12536bc6b55
Diego Mejía <57368067+Yibizo@users.noreply.github.com>
2021-04-28 12:08:49 -0500

    Update MainMenu.cs

--- videogame/Assets/Scripts/Core/MainMenu.cs
+++ videogame/Assets/Scripts/Core/MainMenu.cs
@@ -1,4 +1,17 @@
-﻿using System.Collections;
+﻿/*
+Authors:
+    - Jorge Cabiedes (A01024053)
+    - Diego Mejía (A01024228)
+    - Enrique Mondelli (A01379363)
+    - José Salgado (A01023661)
+    
+Modification Date: 27/04/21
+
+Functionality: 
+    This script is used to load a new game and the credits from the title screen
+*/
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -9,11 +22,13 @@ public class MainMenu : MonoBehaviour
     public string newGameScene;
     public string creditsScene;
 
+    //load new game
     public void NewGame()
     {
         SceneManager.LoadScene(newGameScene);
     }
 
+    //load credits scene
     public void Credits()
     {
         SceneManager.LoadScene(creditsScene);
