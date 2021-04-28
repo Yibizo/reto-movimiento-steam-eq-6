08e3621bfe69ee66881863487b519eff030fb9f7
Diego Mejía <57368067+Yibizo@users.noreply.github.com>
2021-04-28 12:06:04 -0500

    Update LevelGate.cs

--- videogame/Assets/Scripts/Gameplay/LevelGate.cs
+++ videogame/Assets/Scripts/Gameplay/LevelGate.cs
@@ -1,16 +1,32 @@
-﻿using System;
+﻿/*
+Authors:
+    - Jorge Cabiedes (A01024053)
+    - Diego Mejía (A01024228)
+    - Enrique Mondelli (A01379363)
+    - José Salgado (A01023661)
+    
+Modification Date: 15/04/21
+
+Functionality: 
+    This script involves all level gate logic when interacted with
+*/
+
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class LevelGate : MonoBehaviour, Interactable
 {
+    //define dialog and required level to access
     [SerializeField] Dialog dialog;
     [SerializeField] Dialog dialogSuccess;
     [SerializeField] Dialog dialogFailure;
 
     [SerializeField] int reqlevel;
 
+    //function that when interacted, get player program and check if player is equal or higher to the required level
+    //if true, disable game object
     public void Interact()
     {
         var playerParty = FindObjectOfType<ProgramParty>();
@@ -21,6 +37,7 @@ public class LevelGate : MonoBehaviour, Interactable
         
     }
 
+    //function that displays dialog and checks level
     IEnumerator Dialog(Program playerProgram)
     {
         yield return DialogManager.Instance.ShowDialog(dialog);
@@ -28,6 +45,7 @@ public class LevelGate : MonoBehaviour, Interactable
         Checker(playerProgram);
     }
 
+    //function to check level and define gate action
     void Checker(Program playerProgram)
     {
         if (playerProgram.Level >= reqlevel)
