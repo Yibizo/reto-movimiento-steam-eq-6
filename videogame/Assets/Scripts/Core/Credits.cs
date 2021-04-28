321b8aefc21227d04b6a40e136241057547b192b
Diego Mejía <57368067+Yibizo@users.noreply.github.com>
2021-04-28 12:07:49 -0500

    Update Credits.cs

--- videogame/Assets/Scripts/Core/Credits.cs
+++ videogame/Assets/Scripts/Core/Credits.cs
@@ -1,4 +1,15 @@
-﻿using System.Collections;
+﻿/*
+Authors:
+    - Jorge Cabiedes (A01024053)
+    - Diego Mejía (A01024228)
+    - Enrique Mondelli (A01379363)
+    - José Salgado (A01023661)
+Modification Date: 27/04/21
+Functionality: 
+    This script is used to load title screen
+*/
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -7,6 +18,7 @@ public class Credits : MonoBehaviour
 {
     public string titleScene;
 
+    //load title screen scene
     public void TitleScreen()
     {
         SceneManager.LoadScene(titleScene);
